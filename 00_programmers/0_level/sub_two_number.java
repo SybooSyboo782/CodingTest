ff424c6fe48d692d9f022b8374a23a822369d74b
syboosyboo <john782@ajou.ac.kr>
2023-04-25 21:12:43 +0900

    코드 실습 : 0레벨 두 수의 차 구하기 실습

---
+++ 00_programmers/0_level/sub_two_number.java
@@ -0,0 +1,7 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/120803 에서 실행하기 
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = num1 - num2;
+        return answer;
+    }
+}
