58a149f476eedc5a190c01b4a00fc7e97640069f
syboosyboo <john782@ajou.ac.kr>
2023-05-06 14:21:32 +0900

    실습 : 문자열을 정수로 바꾸기 1레벨 실습

---
+++ 00_programmers/1_level/string_to_int.java
@@ -0,0 +1,8 @@
+// 문자열을 정수로 바꾸기 https://school.programmers.co.kr/learn/courses/30/lessons/12925 
+class Solution {
+    public int solution(String s) {
+        
+        return Integer.parseInt(s);
+        
+    }
+}
