92512fdf0d39f2e2e56fff2d63858e590b682a77
syboosyboo <john782@ajou.ac.kr>
2023-05-30 06:52:37 +0900

    실습 : 프로그래머스 1레벨 같은 숫자는 싫어 실습

---
+++ 00_programmers/1_level/not_same_number.java
@@ -0,0 +1,28 @@
+// 같은 숫자는 싫어 https://school.programmers.co.kr/learn/courses/30/lessons/12906
+
+import java.util.*;
+
+public class Solution {
+    public int[] solution(int []arr) {
+        
+        Stack<Integer> stack = new Stack<>(); 
+        
+        for(int i = 0; i < arr.length; i++){
+            
+            if(stack.isEmpty()) {
+                stack.push(arr[i]);
+            }
+            
+            if(stack.peek() != arr[i]) {
+                stack.push(arr[i]);
+            }
+        }
+        
+        int[] answer = new int[stack.size()];
+        for(int i = stack.size() - 1; i >= 0; i--) {
+            answer[i] = stack.pop();
+        }
+        
+        return answer;
+    }
+}
