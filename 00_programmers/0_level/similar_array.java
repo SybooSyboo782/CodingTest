b1eeb959d79aeedd4d1066fdff94a2977a3c5e3d
syboosyboo <john782@ajou.ac.kr>
2023-07-27 21:23:31 +0900

    실습 : 프로그래머스 0레벨 배열의 유사도 실습

---
+++ 00_programmers/0_level/similar_array.java
@@ -0,0 +1,20 @@
+// 배열의 유사도 https://school.programmers.co.kr/learn/courses/30/lessons/120903
+
+import java.util.*;
+
+class Solution {
+    public int solution(String[] s1, String[] s2) {
+        
+        int answer = 0;
+        
+        for(int i = 0; i < s1.length; i++) {
+            for(int j = 0; j < s2.length; j++) {
+                if(s1[i].equals(s2[j])) {
+                    answer++;
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
