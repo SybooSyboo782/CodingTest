6f7e072ca661fa3f2f55730af6324ccb8f879b37
syboosyboo <john782@ajou.ac.kr>
2023-08-11 11:24:45 +0900

    실습 : 프로그래머스 0레벨 공배수 실습

---
+++ 00_programmers/0_level/common_multiple.java
@@ -0,0 +1,12 @@
+// 공배수 https://school.programmers.co.kr/learn/courses/30/lessons/181936
+class Solution {
+    public int solution(int number, int n, int m) {
+
+        int answer = 0;
+        if(number % n == 0 && number % m == 0) {
+            answer = 1;
+        } else {;}
+
+        return answer;
+    }
+}
