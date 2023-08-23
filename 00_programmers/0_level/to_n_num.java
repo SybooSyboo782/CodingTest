b3bbd8a29df0b6633fab65a6a813e23835a56f52
syboosyboo <john782@ajou.ac.kr>
2023-08-23 11:37:09 +0900

    실습 : 프로그래머스 0레벨 n 번째 원소까지 실습

---
+++ 00_programmers/0_level/to_n_num.java
@@ -0,0 +1,13 @@
+// n 번째 원소까지 https://school.programmers.co.kr/learn/courses/30/lessons/181889
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+
+        int[] answer = new int[n];
+
+        for(int i = 0; i < n; i++) {
+            answer[i] = num_list[i];
+        }
+
+        return answer;
+    }
+}
