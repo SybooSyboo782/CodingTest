d7c1d2d4a14ce5410c547c9e6b6c3c55732711dc
syboosyboo <john782@ajou.ac.kr>
2023-09-08 23:22:12 +0900

    실습 : 프로그래머스 0레벨 소문자로 바꾸기 실습

---
+++ 00_programmers/0_level/to_lower_case.java
@@ -0,0 +1,7 @@
+// 소문자로 바꾸기 https://school.programmers.co.kr/learn/courses/30/lessons/181876
+class Solution {
+    public String solution(String myString) {
+                
+        return myString.toLowerCase();
+    }
+}
