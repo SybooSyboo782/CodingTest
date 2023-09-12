a9a51cc79eaa9e3b139cb129c40667b2c0bc7cf4
syboosyboo <john782@ajou.ac.kr>
2023-09-12 14:15:46 +0900

    실습 : 프로그래머스 0레벨 접두사인지 확인하기 실습

---
+++ 00_programmers/0_level/is_prefix.java
@@ -0,0 +1,11 @@
+// 접두사인지 확인하기 https://school.programmers.co.kr/learn/courses/30/lessons/181906
+class Solution {
+    public int solution(String my_string, String is_prefix) {
+        
+        if (my_string.startsWith(is_prefix)) {
+            return 1;
+        } else {
+            return 0;
+        }        
+    }
+}
