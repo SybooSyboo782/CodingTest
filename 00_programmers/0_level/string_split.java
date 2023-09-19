5af6bc534bb804874b0c72665eb9283113365330
syboosyboo <john782@ajou.ac.kr>
2023-09-19 22:32:54 +0900

    실습 : 프로그래머스 0레벨 공백으로 구분하기 1 실습

---
+++ 00_programmers/0_level/string_split.java
@@ -0,0 +1,7 @@
+// 공백으로 구분하기 1 https://school.programmers.co.kr/learn/courses/30/lessons/181869
+class Solution {
+    public String[] solution(String my_string) {
+    
+        return my_string.split(" ");
+    }
+}
