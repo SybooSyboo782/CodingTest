4770bb6bcab5c192f967c227f42ac08117a053bc
SybooSyboo782 <111809392+SybooSyboo782@users.noreply.github.com>
2023-10-05 16:55:34 +0900

    실습 : 프로그래머스 0레벨 세균 증식 실습

---
+++ 00_programmers/0_level/bacterial_growth.java
@@ -0,0 +1,9 @@
+// 세균 증식 https://school.programmers.co.kr/learn/courses/30/lessons/120910
+class Solution {
+    public int solution(int n, int t) {
+        
+        int answer = (int)Math.pow(2, t) * n;
+        
+        return answer;
+    }
+}
