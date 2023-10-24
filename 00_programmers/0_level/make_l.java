fa82c35859771df8fa206c980f6c31b56419b9c1
syboosyboo <john782@ajou.ac.kr>
2023-10-24 22:50:54 +0900

    실습 : 프로그래머스 0레벨 l로 만들기

---
+++ 00_programmers/0_level/make_l.java
@@ -0,0 +1,7 @@
+// l로 만들기 https://school.programmers.co.kr/learn/courses/30/lessons/181834
+class Solution {
+    public String solution(String myString) {
+        
+        return myString.replaceAll("[a-k]", "l");
+    }
+}
