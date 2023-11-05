60381c99db661f25d9fbd8cc49116157ee983939
syboosyboo <john782@ajou.ac.kr>
2023-11-05 22:14:26 +0900

    실습 : 프로그래머스 0레벨 n의 배수 고르기 실습

---
+++ 00_programmers/0_level/pick_n.java
@@ -0,0 +1,22 @@
+// n의 배수 고르기 https://school.programmers.co.kr/learn/courses/30/lessons/120905
+class Solution {
+    public int[] solution(int n, int[] numlist) {
+        
+        int[] array = new int[numlist.length];
+        int count = 0;
+        
+        for (int i = 0; i < numlist.length; i++){
+            if(numlist[i] % n == 0) {
+                array[count] = numlist[i];
+                count++;
+            }
+        }
+        
+        int[] answer = new int[count];
+        for(int i = 0; i < count; i++) {
+            answer[i] = array[i];
+        }
+        
+        return answer;
+    }
+}
