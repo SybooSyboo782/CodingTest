d58371675a0fb7449032f2d668a68317c625d200
syboosyboo <john782@ajou.ac.kr>
2023-12-07 22:19:57 +0900

    실습 : 프로그래머스 0레벨 덧셈식 출력하기 실습

---
+++ 00_programmers/0_level/print_sum.java
@@ -0,0 +1,12 @@
+// 덧셈식 출력하기 https://school.programmers.co.kr/learn/courses/30/lessons/181947
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.printf("%d + %d = %d", a, b, a + b);
+    }
+}
