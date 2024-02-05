a44acdb7a1baa27a3f92c901b295217f0f095e68
syboosyboo <john782@ajou.ac.kr>
2024-02-05 23:07:36 +0900

    실습 : 프로그래머스 0레벨 문자열 반복해서 출력하기 실습

---
+++ 00_programmers/0_level/repeat_string.java
@@ -0,0 +1,18 @@
+// 문자열 반복해서 출력하기 https://school.programmers.co.kr/learn/courses/30/lessons/181950
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        int n = sc.nextInt();
+        String answer = "";
+        
+        for(int i = 0; i < n; i++) {
+            answer += str;
+        }
+        
+        System.out.print(answer);
+    }
+}
