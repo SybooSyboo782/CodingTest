1927ef8410b15325ee7e5e749ccda2481f03b847
syboosyboo <john782@ajou.ac.kr>
2024-02-23 22:57:47 +0900

    실습 : 프로그래머스 0레벨 공 던지기 실습

---
+++ 00_programmers/0_level/throw_ball.java
@@ -0,0 +1,17 @@
+// 공 던지기 https://school.programmers.co.kr/learn/courses/30/lessons/120843
+class Solution {
+    public int solution(int[] numbers, int k) {
+        
+        int answer = 0;
+        int index = 0;
+        
+        for (int i = 1 ; i < k; i++) {
+            index += 2;
+        }
+        
+        index %= numbers.length;
+        answer = numbers[index];
+        
+        return answer;
+    }
+}
